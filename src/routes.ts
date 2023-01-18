8e2fc05c3489617c35b4f2baf614d5f628ededa6
Eduardo Muchak <eduardomuchak@gmail.com>
2023-01-18 09:49:11 -0300

    feat: :sparkles: rota de criacao de um novo habito

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,32 @@
-import { FastifyInstance } from "fastify";
-import { prisma } from "./lib/prisma";
+import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+import { prisma } from './lib/prisma';
+import dayjs from 'dayjs';
 
 export async function appRoutes(app: FastifyInstance) {
-  app.get("/", async () => {
-    const habits = await prisma.habit.findMany();
-    return habits;
+  app.post('/habits', async (request) => {
+    // Validate request body
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6)),
+    });
+
+    const { title, weekDays } = createHabitBody.parse(request.body);
+
+    const today = dayjs().startOf('day').toDate();
+
+    await prisma.habit.create({
+      data: {
+        title,
+        created_at: today,
+        weekDays: {
+          create: weekDays.map((day) => {
+            return {
+              week_day: day,
+            };
+          }),
+        },
+      },
+    });
   });
 }
