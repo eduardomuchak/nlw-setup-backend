bd78e6426d67c136d1df832d5d78fa96d5d6c7f4
Eduardo Muchak <eduardomuchak@gmail.com>
2023-01-21 11:37:41 -0300

    fix: :bug: correcao arquivo seed e atualizacao dependencias prisma

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
-import 'dotenv/config';
-import Fastify from "fastify";
-import cors from "@fastify/cors";
+import Fastify from 'fastify';
+import cors from '@fastify/cors';
 import { appRoutes } from './routes';
 
-const PORT = Number(process.env.PORT) || 3001;
 const app = Fastify();
 
 app.register(cors);
 app.register(appRoutes);
 
-app.listen({ port: PORT }, () => console.log(`Running on port ${PORT}`));
+app
+  .listen({
+    port: 8080,
+  })
+  .then(() => {
+    console.log('HTTP Server running on Port 8080!');
+  });
