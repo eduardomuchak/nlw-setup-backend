bd567457ba2fdc7a684356f59faed1594b349504
Eduardo Muchak <eduardomuchak@gmail.com>
2023-01-25 21:30:23 -0300

    test: tentativa deploy

--- src/server.ts
+++ src/server.ts
@@ -9,15 +9,13 @@ app.register(cors, {
 });
 app.register(appRoutes);
 
-const ip = require('ip');
+// const ip = require('ip');
 
 app
   .listen({
     port: Number(process.env.PORT),
-    host: ip.address(),
+    // host: ip.address(),
   })
   .then(() => {
-    console.log(
-      `HTTP Server running on Port ${ip.address()}:${process.env.PORT}`,
-    );
+    console.log(`HTTP Server running on Port:${process.env.PORT}`);
   });
